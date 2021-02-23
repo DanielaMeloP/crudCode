6270a033ef6e7effd35c71e6c3e85273a822b8a3
Daniela M <daniela.paz.me@gmail.com>
2021-02-22 21:36:58 -0300

    Agregar y listar ok

--- app/Views/listado.php
+++ app/Views/listado.php
@@ -83,9 +83,10 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
-    <!-- Script sweetalert   -->
+    <!-- Import script sweetalert   -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
+    <!-- Configuracion alerta   -->              
     <script type="text/javascript">
       let mensaje = '<?php echo $mensaje ?>';
       if (mensaje == '1' ) {
